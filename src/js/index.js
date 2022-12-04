906349abbcf41446ee76eccb1f5820549705181d
pashakiz <pashakiz@gmail.com>
2022-12-04 05:32:43 +0300

    init (add bootstrap)

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,28 @@
 import '@scss/main.scss'
 
-const foo = bar => console.log('some...', bar)
+const foo = bar => console.log('some...', bar);
+foo('bar');
 
-foo('bar')
+
+//preloader
+const preloader = () => {
+  setTimeout(function () {
+    let preloader = document.body.querySelector('#page-preloader');
+    if (preloader != null) {
+      if (!preloader.classList.contains('done')) {
+        preloader.classList.add('done');
+      }
+    }
+  }, 1000);
+}
+document.addEventListener('DOMContentLoaded', preloader, false);
+
+//upload photo
+const uploadPhotoInput = document.querySelector('.custom-file-input');
+const profilePhoto = document.querySelector('.profile-photo');
+const uploadPhoto = () => {
+  let url = 'img/photos/ava.jpg';
+  profilePhoto.style.backgroundImage = 'url(' + url + ')';
+  console.log('uploadPhoto');
+}
+uploadPhotoInput.addEventListener('change', uploadPhoto, false);
